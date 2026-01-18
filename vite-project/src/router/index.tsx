3b9556a23961a4baaded9ba471c1314b269e23c1
nabinur <nabinursh123@gmail.com>
2026-01-18 17:59:39 +0500

    fix: router paths and tailwind config

--- vite-project/src/router/index.tsx
+++ vite-project/src/router/index.tsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Home from '../Pages/home';
-import Blog from '../Pages/home/blog';
+import Home from '../Pages/Home';
+import Blog from '../Pages/Blog';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
   },
-   {
+  {
     path: '/blog',
-    element: <Blog />, 
-  }, 
+    element: <Blog />,
+  },
 ]); 
