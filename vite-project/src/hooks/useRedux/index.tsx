054f8ca8165f390c81fd6389c2ae1c5447603c65
nabinur <nabinursh123@gmail.com>
2026-02-02 04:00:08 +0500

    imtixon

--- vite-project/src/hooks/useRedux/index.tsx
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "../../redux";
-
-export const useReduxSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useReduxDispatch = () => useDispatch<AppDispatch>();
